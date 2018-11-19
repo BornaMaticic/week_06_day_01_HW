1e7913794f7ecfb0c436728309c729b3b0660644
Borna Maticic <bornamaticic@yahoo.com>
2018-11-19 23:48:11 +0000

    Homework completed.

---
+++ hw_scope_whodunnit/whodoneit.js
@@ -0,0 +1,163 @@
+// #### Episode 1
+
+// const scenario = {
+//   murderer: 'Miss Scarlet',
+//   room: 'Library',
+//   weapon: 'Rope'
+// };
+//
+// const declareMurderer = function() {
+//   return `The murderer is ${scenario.murderer}.`;
+// }
+//
+// const verdict = declareMurderer();
+// console.log(verdict);
+
+Predicted output | Miss Scarlet is the  murderer
+The function will take the name from the scenario object. It all looks good.
+
+
+// #### Episode 2
+
+
+// const murderer = 'Professor Plum';
+//
+// const changeMurderer = function() {
+//   murderer = 'Mrs. Peacock';
+// }
+//
+// const declareMurderer = function() {
+//   return `The murderer is ${murderer}.`;
+// }
+//
+// changeMurderer();
+// const verdict = declareMurderer();
+// console.log(verdict);
+//
+Predicted output | Program will try to change the murderer but const can't be changed.
+
+
+
+// #### Episode 3
+
+
+// let murderer = 'Professor Plum';
+//
+// const declareMurderer = function() {
+//   let murderer = 'Mrs. Peacock';
+//   return `The murderer is ${murderer}.`;
+// }
+//
+// const firstVerdict = declareMurderer();
+// console.log('First Verdict: ', firstVerdict);
+//
+// const secondVerdict = `The murderer is ${murderer}.`;
+// console.log('Second Verdict: ', secondVerdict);
+//
+//
+Predicted output | First murderer is MsPeacock and second is professor Plum because Mrs. Peacock is inside the function and professor Plum is the original murderer(value)
+
+// EPISODE 4
+//
+// let suspectOne = 'Miss Scarlet';
+// let suspectTwo = 'Professor Plum';
+// let suspectThree = 'Mrs. Peacock';
+//
+// const declareAllSuspects = function() {
+//   let suspectThree = 'Colonel Mustard';
+//   return `The suspects are ${suspectOne}, ${suspectTwo}, ${suspectThree}.`;
+// }
+//
+// const suspects = declareAllSuspects();
+// console.log(suspects);
+// console.log(`Suspect three is ${suspectThree}.`);
+//
+Predicted output | The suspects will be Scarlet, Plum and Mustard. Suspect three will be Peacock.
+Same as above,only inside of the scope the third changes.
+
+//
+// #### Episode 5
+
+// const scenario = {
+//   murderer: 'Miss Scarlet',
+//   room: 'Kitchen',
+//   weapon: 'Candle Stick'
+// };
+//
+// const changeWeapon = function(newWeapon) {
+//   scenario.weapon = newWeapon;
+// }
+//
+// const declareWeapon = function() {
+//   return `The weapon is the ${scenario.weapon}.`;
+// }
+//
+// changeWeapon('Revolver');
+// const verdict = declareWeapon();
+// console.log(verdict);
+
+
+Predicted output | The scenario.weapon will be changed to Revolver because scenario will stay the same as a constant but its internal part of a "hash" can be changed.
+
+
+// #### Episode 6
+//
+//
+// let murderer = 'Colonel Mustard';
+//
+// const changeMurderer = function() {
+//   murderer = 'Mr. Green';
+//
+//   const plotTwist = function() {
+//     murderer = 'Mrs. White';
+//   }
+//
+//   plotTwist();
+// }
+//
+// const declareMurderer = function () {
+//   return `The murderer is ${murderer}.`;
+// }
+//
+// changeMurderer();
+// const verdict = declareMurderer();
+// console.log(verdict);
+//
+
+Predicted output | Murderer is Mrs. White.
+I was corect as changeMurderer constant changes it for the whole function until plotTwist is called uppon and ultimately changing it to Mrs.White.
+
+
+// #### Episode 7
+
+//
+// let murderer = 'Professor Plum';
+//
+// const changeMurderer = function() {
+//   murderer = 'Mr. Green';
+//
+//   const plotTwist = function() {
+//     let murderer = 'Colonel Mustard';
+//
+//     const unexpectedOutcome = function() {
+//       murderer = 'Miss Scarlet';
+//     }
+//
+//     unexpectedOutcome();
+//   }
+//
+//   plotTwist();
+// }
+//
+// const declareMurderer = function() {
+//   return `The murderer is ${murderer}.`;
+// }
+//
+// changeMurderer();
+// const verdict = declareMurderer();
+// console.log(verdict);
+//
+
+Predicted output | The murderer is Mr Green
+Let in plotTwist means any change to the variable murderer is ignored outside of the function plotTwist.
+Only change that remains outside of the changeMurder function is in the unexpectedOutcome.
